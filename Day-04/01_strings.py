0fe2d5e8fae5acaef181470e96af9e109629576d
Haritha <gadiharitha2730@gmail.com>
2026-07-04 23:18:52 +0530

    Day 4: Learned Strings and String Indexing

---
+++ Day-04/01_strings.py
@@ -0,0 +1,17 @@
+# Day 4 - Strings
+
+name = "Haritha"
+college = "BIET"
+branch = "CSE"
+city = "Hyderabad"
+
+print("Student Information")
+print("Name:", name)
+print("College:", college)
+print("Branch:", branch)
+print("City:", city)
+
+print(type(name))
+print(type(college))
+print(type(branch))
+print(type(city))

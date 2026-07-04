e118e163d53b90a878af103cb70a1ea01bac242d
Haritha <gadiharitha2730@gmail.com>
2026-07-04 22:56:07 +0530

    Day 3: Learned Arithmetic Operators

---
+++ Dat-03/02_all_operators_program.py
@@ -0,0 +1,12 @@
+# Day 3 - Arithmetic Operators Program
+
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+
+print("Addition:", first + second)
+print("Subtraction:", first - second)
+print("Multiplication:", first * second)
+print("Division:", first / second)
+print("Modulus:", first % second)
+print("Floor Division:", first // second)
+print("Exponent:", first ** second)

ed718453eb802975e7e8781e43e91cb52c29768c
Haritha <gadiharitha2730@gmail.com>
2026-07-07 16:21:52 +0530

    updated string indexing

---
+++ Day-04/02_string_indexing.py
@@ -0,0 +1,8 @@
+# Day 4 - String Indexing
+
+name = "Haritha"
+
+print("First Character:", name[0])
+print("Third Character:", name[2])
+print("Last Character:", name[-1])
+print("Second Last Character:", name[-2])

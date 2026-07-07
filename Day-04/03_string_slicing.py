376a879d12311db37e28cf5be7279cfd1982b638
Haritha <gadiharitha2730@gmail.com>
2026-07-07 16:15:02 +0530

    Day 4: Learned Strings, String Indexing and String Slicing

---
+++ Day-04/03_string_slicing.py
@@ -0,0 +1,9 @@
+# Day 5 - String Slicing
+
+name = "Haritha"
+
+print("First 4 Characters:", name[:4])
+print("Last 4 Characters:", name[3:])
+print("Characters from index 1 to 5:", name[1:5])
+print("Middle Characters:", name[1:6])
+print("Complete String:", name[:])

1439339b8ce9bf10fe42254a94882316e7b3fd33
Haritha <gadiharitha2730@gmail.com>
2026-07-08 14:05:33 +0530

    Day 5: Learned String Methods

---
+++ Day-05/Day-05 └── 01_string_methods.py
@@ -0,0 +1,10 @@
+# Day 5 - String Methods
+
+name = "Haritha"
+
+print("Original String:", name)
+print("After upper():", name.upper())
+print("After lower():", name.lower())
+print("After strip():", name.strip())
+print("After replace():", name.replace("a", "A"))
+print("Index of t:", name.find("t"))

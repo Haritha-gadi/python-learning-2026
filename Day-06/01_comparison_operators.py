1469802c5097ce7a330441cb224db49b97b5b9e4
Haritha <gadiharitha2730@gmail.com>
2026-07-08 14:09:58 +0530

    Day 6: Learned Comparison and Logical Operators

---
+++ Day-06/01_comparison_operators.py
@@ -0,0 +1,11 @@
+# Day 6 - Comparison (Relational) Operators
+
+age = 20
+
+print("Comparison Operator Results")
+print("age == 20:", age == 20)
+print("age != 18:", age != 18)
+print("age > 18:", age > 18)
+print("age < 25:", age < 25)
+print("age >= 20:", age >= 20)
+print("age <= 21:", age <= 21)
---
+++ Day-06/02_logical_operators.py
@@ -0,0 +1,9 @@
+# Day 6 - Logical Operators
+
+age = 20
+marks = 75
+
+print("Logical Operator Results")
+print("age > 18 and marks > 35:", age > 18 and marks > 35)
+print("age < 18 or marks > 35:", age < 18 or marks > 35)
+print("not(age < 18):", not(age < 18))

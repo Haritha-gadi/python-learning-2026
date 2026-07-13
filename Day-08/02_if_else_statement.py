44acafa5ff1631f388e35b0687b6339edec6d002
Haritha <gadiharitha2730@gmail.com>
2026-07-13 16:55:44 +0530

    Day 8: Learned if, if-else, and elif statements

---
+++ Day-08/02_if_else_statement.py
@@ -0,0 +1,10 @@
+# Day 8 - If-Else Statement
+
+marks = 30
+
+print("If-Else Statement Results")
+
+if marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
---
+++ Day-08/03_elif_statement.py
@@ -0,0 +1,14 @@
+# Day 8 - Elif Statement
+
+marks = 82
+
+print("Elif Statement Results")
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
